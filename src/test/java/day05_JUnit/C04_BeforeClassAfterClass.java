22b2177a08daf116b0b25e316a1af74063a5e2cd
kkbraslnn <kkaratash@icloud.com>
2023-05-26 23:05:32 +0200

    v1.2

--- src/test/java/day05_JUnit/C04_BeforeClassAfterClass.java
+++ src/test/java/day05_JUnit/C04_BeforeClassAfterClass.java
@@ -16,30 +16,43 @@ public class C04_BeforeClassAfterClass {
      */
     @BeforeClass
     public static void setUp(){
-        System.out.println("Butun testlerden once calsiti");
+       /* System.out.println("Butun testlerden once calisti");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        */
     }
     @AfterClass
     public static void tearDown(){
         System.out.println("Butun testlerden sonra calisti");
+        System.out.println("..............................");
     }
     @Before
     public void setUp01(){
-        System.out.println("her testten once calsir");
+        System.out.println("her testten once calisir");
+        System.out.println(".......................");
     }
     @After
     public void tearDown01(){
         System.out.println("her testten sonra calisir");
+        System.out.println(".......................");
     }
     @Test
     public void test01(){
         System.out.println("ilk test");
+        System.out.println(".......................");
     }
     @Test
     public void test02(){
         System.out.println("ikinci test");
+        System.out.println(".......................");
+    }
+    @Test
+    @Ignore //eger bir testi calistirmak istemezsek bu sekilde kullanabiliriz
+    public void test03(){
+        System.out.println("ucuncu test");
+
     }
 }
